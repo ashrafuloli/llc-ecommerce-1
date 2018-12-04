847bf1502a661406afd62d0550cee91ada01e66a
Muhammad Sumon Molla Selim <smseleem@gmail.com>
2018-12-04 20:31:30 +0600

    models added

---
+++ app/Models/Category.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Category extends Model
+{
+    protected $guarded = [];
+
+    public function parent_category(): \Illuminate\Database\Eloquent\Relations\BelongsTo
+    {
+        return $this->belongsTo(__CLASS__);
+    }
+
+    public function child_category(): \Illuminate\Database\Eloquent\Relations\HasMany
+    {
+        return $this->hasMany(__CLASS__);
+    }
+
+    public function products(): \Illuminate\Database\Eloquent\Relations\HasMany
+    {
+        return $this->hasMany(Product::class);
+    }
+}
